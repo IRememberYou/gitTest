22aa47b5da6c79b7f2837708c1f68678935894e5
wangjianping <wangjianping@pyhtech.cn>
2020-03-12 11:02:28 +0800

    feature 功能开发完成

---
+++ app/src/main/java/com/xky/emptyproject/ui/A.java
@@ -0,0 +1,9 @@
+package com.xky.emptyproject.ui;
+
+import android.util.Log;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("feature_1.0.0 版本调整");
+    }
+}
---
+++ app/src/main/java/com/xky/emptyproject/ui/B.java
@@ -0,0 +1,8 @@
+package com.xky.emptyproject.ui;
+
+public class B {
+
+    public static void main(String[] args) {
+        System.out.println("feature_1.0.0 B版本调整_____");
+    }
+}
