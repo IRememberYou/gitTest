15b70f33d72822cf4a05f53e0d2c3af0e901cf58
wangjianping <wangjianping@pyhtech.cn>
2020-03-12 11:55:58 +0800

    feature1.3.0 提交代码。。。。。

--- app/src/main/java/com/xky/emptyproject/ui/F.java
+++ app/src/main/java/com/xky/emptyproject/ui/F.java
@@ -4,6 +4,8 @@ import java.sql.SQLOutput;
 
 public class F {
     public static void main(String[] args) {
+        System.out.println("增加逻辑。。。。。。");
         System.out.println("feature1.2.0  第二次提交代码。。。。。");
+        System.out.println("feature_1.3.0 分支开发。。。。");
     }
 }
---
+++ app/src/main/java/com/xky/emptyproject/ui/H.java
@@ -0,0 +1,7 @@
+package com.xky.emptyproject.ui;
+
+public class H {
+    public static void main(String[] args) {
+        System.out.println("feature_1.3.0 新功能。。。。。");
+    }
+}
