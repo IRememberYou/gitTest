696db22efcd467d3b2593857b64e21d64eb4fda3
wangjianping <wangjianping@pyhtech.cn>
2020-03-12 11:26:04 +0800

    feature1.1.0 功能开发

--- app/src/main/java/com/xky/emptyproject/ui/B.java
+++ app/src/main/java/com/xky/emptyproject/ui/B.java
@@ -3,6 +3,7 @@ package com.xky.emptyproject.ui;
 public class B {
 
     public static void main(String[] args) {
-        System.out.println("feature_1.0.0 B版本调整_____");
+//        System.out.println("feature_1.0.0 B版本调整_____");
+        System.out.println("feature_1.1.0 分支修改");
     }
 }
---
+++ app/src/main/java/com/xky/emptyproject/ui/C.java
@@ -0,0 +1,7 @@
+package com.xky.emptyproject.ui;
+
+public class C {
+    public static void main(String[] args) {
+        System.out.println("feature1.1.0  分支新建。。");
+    }
+}
