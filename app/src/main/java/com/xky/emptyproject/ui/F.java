6301733d2439f10cd017d9fb9762876363d0adc2
wangjianping <wangjianping@pyhtech.cn>
2020-03-12 11:47:42 +0800

    feature1.3.1 提交代码。。。。。

--- app/src/main/java/com/xky/emptyproject/ui/F.java
+++ app/src/main/java/com/xky/emptyproject/ui/F.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class F {
     public static void main(String[] args) {
         System.out.println("feature1.2.0  第二次提交代码。。。。。");
+        System.out.println("feature_1.3.1  分支修改aaa");
     }
 }
