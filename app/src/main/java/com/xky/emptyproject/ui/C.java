76bb9336cf5d4554c0759a157178b2b22793724a
wangjianping <wangjianping@pyhtech.cn>
2020-03-12 11:36:41 +0800

    feature 第二次提交代码。。。。。

--- app/src/main/java/com/xky/emptyproject/ui/C.java
+++ app/src/main/java/com/xky/emptyproject/ui/C.java
@@ -3,5 +3,6 @@ package com.xky.emptyproject.ui;
 public class C {
     public static void main(String[] args) {
         System.out.println("feature1.1.0  分支新建。。");
+        System.out.println("feature1.2.0 分支开发。。。。。。");
     }
 }
---
+++ app/src/main/java/com/xky/emptyproject/ui/F.java
@@ -0,0 +1,9 @@
+package com.xky.emptyproject.ui;
+
+import java.sql.SQLOutput;
+
+public class F {
+    public static void main(String[] args) {
+        System.out.println("feature1.2.0  第二次提交代码。。。。。");
+    }
+}
